983e76fd6ee21e7fe2f5b580683dfa54988acef6
maulairfani <maulairfani15@gmail.com>
2022-10-09 16:50:53 +0700

    final commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let popup = document.getElementsByClassName('popup')[0]
+
+const showPopup = () => {
+    popup.style.display = 'flex'
+}
+
+const closePopup = () => {
+    popup.style.display = 'none'
+}
+
+
+
